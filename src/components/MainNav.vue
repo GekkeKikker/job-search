c8db83259cc52f8d26c609b03b110f5959af924e
GekkeKikker <15897712+GekkeKikker@users.noreply.github.com>
2025-01-25 08:21:10 +0100

    changes until tailwind integration

---
+++ src/components/MainNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="w-full text-sm"></header>
+  <div class="fixed top-0 left-0 h-16 w-full bg-white">
+    <div
+      class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 my-2 mx-auto"
+    >
+      <a href="/" class="flex h-full item-center text-xl">{{ company }}</a>
+      <h2 class="ml-8 flex h-full items-center">Developed by {{ author }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "Bobo Careers",
+      author: "Maarten Alles",
+    };
+  },
+};
+</script>
---
+++ src/playground.js
@@ -0,0 +1,4 @@
+const fruits = ["apple", "banana", "orange"];
+const vegetables = ["Cucumber", "Radish"];
+
+console.log([...fruits, ...vegetables]);
