e5674821a6aa987c888d86510b62e82e4e08ece5
GekkeKikker <15897712+GekkeKikker@users.noreply.github.com>
2025-03-07 18:51:48 +0100

    t.e.m. les 163

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,24 +1,28 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
+    <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-start-1 col-span-1 border border-blue-700">1 column</div>
-        <div class="col-start-2 col-span-5 border border-blue-700">5 columns</div>
-        <div class="col-start-7 col-span-5 border border-blue-700">5 columns</div>
-        <div class="col-start-12 col-span-1 border border-blue-700">1 column</div>
+        <div class="col-start-1 col-span-1"></div>
+        <div class="col-start-2 col-span-5">
+          <the-headline />
+          <job-search-form />
+        </div>
+        <div class="col-start-7 col-span-5"></div>
+        <div class="col-start-12 col-span-1"></div>
       </div>
     </section>
   </main>
 </template>
 
 <script>
+import TheHeadline from "@/components/TheHeadline.vue";
+import JobSearchForm from "@/components/JobSearchForm.vue";
+
 export default {
   name: "TheHero",
-  beforeCreate() {
-    console.log("Component is about to be created");
-  },
-  created() {
-    console.log("Component has been created");
+  components: {
+    TheHeadline,
+    JobSearchForm,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
+// custom styles
+import "@/index.css";
+
+// the app
 import { createApp } from "vue";
+import App from "@/App.vue";
+
+// FontAwesome icons
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-import "@/index.css";
-import App from "@/App.vue";
-
 library.add(faSearch);
 
-createApp(App).component("font-awsome-icon", FontAwesomeIcon).mount("#app");
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
